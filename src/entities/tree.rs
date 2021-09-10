34f0802a31ba226f145c1e2d0c2cbf7f3d67ec32
Michael <hellercmichael@gmail.com>
2021-09-09 22:49:07 -0400

    Create tree entity in entities module.

---
+++ src/entities/tree.rs
@@ -0,0 +1,75 @@
+use crate::grid::Grid;
+use crate::forest::Forest;
+
+use rand::Rng;
+use rand::seq::SliceRandom;
+
+#[derive(PartialEq)]
+enum TreeKind {
+    Sapling,
+    Mature,
+    Elder,
+}
+
+struct Tree {
+    age: u32,
+    kind: TreeKind,
+}
+
+impl Tree {
+    const SAPLING_SPAWN_CHANCE: u32 = 0;
+    const MATURE_SPAWN_CHANCE: u32 = 10;
+    const ELDER_SPAWN_CHANCE: u32 = 20;
+
+    const SAPLING_GROW_AGE: u32 = 12;
+    const MATURE_GROW_AGE: u32 = 120;
+
+    fn new(kind: TreeKind) -> Self {
+        Self {
+            age: 0,
+            kind
+        }
+    }
+
+    fn get_spawn_chance(&self) -> u32 {
+        if self.kind == TreeKind::Mature {
+            Tree::MATURE_SPAWN_CHANCE
+        } else if self.kind == TreeKind::Elder {
+            Tree::ELDER_SPAWN_CHANCE
+        } else {
+            Tree::SAPLING_SPAWN_CHANCE
+        }
+    }
+
+    fn grow(&mut self) {
+        if self.age == Tree::SAPLING_GROW_AGE {
+            self.kind = TreeKind::Mature;
+        } else if self.age == Tree::MATURE_GROW_AGE{
+            self.kind = TreeKind::Elder;
+        }
+    }
+
+    // @TODO Instead of passing a mutable grid, perhaps we should return an
+    // Option for the Forest to place itself.
+    fn spawn_sapling(&self, idx: usize, grid: &mut Grid<Option<u32>>) {
+        let mut rng = rand::thread_rng();
+
+        let chance = self.get_spawn_chance();
+        let choice = rng.gen_range(0..100);
+
+        if choice < chance {
+            let mut adjacent_cells = grid.get_adjacent_cells(idx);
+
+            adjacent_cells.shuffle(&mut rng);
+
+            for cell in adjacent_cells {
+                let idx = grid.to_index(cell.x, cell.y);
+                if let None = grid.data[idx] {
+                    grid.place(Some(Forest::SAPLING), cell.x, cell.y);
+
+                    break;
+                }
+            }
+        }
+    }
+}
