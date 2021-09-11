52d441ef890ebf741fdf4ded618ef784b1480a93
Michael <hellercmichael@gmail.com>
2021-09-10 20:48:31 -0400

    Start skeleton for Lumberjack.

---
+++ src/entities/lumberjack.rs
@@ -0,0 +1,43 @@
+use crate::entities::entity::Entity;
+use crate::grid::Grid;
+
+#[derive(Clone)]
+struct Lumberjack {
+    age: u32,
+    // lumber: u32,
+}
+
+impl Lumberjack {
+    fn new() -> Self {
+        Self {
+            age: 0,
+            // lumber: 0,
+        }
+    }
+
+    fn harvest(&self) {
+        // Lumberjacks harvest Trees that they wander into and gain lumber.
+        // Once they've harvested a Tree, the cannot wander anymore.
+
+        // Lumberjacks do not cut down Saplings, only Trees and Elder Trees.
+
+        // Lumberjacks gain 1 piece of lumber per Tree, and 2 pieces per Elder Tree.
+    }
+
+    fn wander(&self) -> Option<usize> {
+        // Lumberjacks wander randomly three times at the end of a month.
+
+        None
+    }
+}
+
+impl Entity for Lumberjack {
+    fn get_symbol(&self) -> &str {
+        "l"
+    }
+
+    fn update(&self, idx: usize, grid: &mut Grid<Option<Box<dyn Entity>>>) {
+        // @TODO Wander to a spot. Perhaps return Option.
+        // @TODO Harvest from the spot wandered to.
+    }
+}
