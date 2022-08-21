921deac1b8ea0511cf85dd247a8f97c8f0d67d5d
Michael <hellercmichael@gmail.com>
2022-08-21 13:16:06 -0400

    Handle drawing larger cell sizes onto the canvas.

--- forest-gui/src/components/App.tsx
+++ forest-gui/src/components/App.tsx
@@ -3,30 +3,35 @@ import { get_forest, update_forest } from '../commands/forest_commands';
 import './App.css';
 
 const DEFAULT_SEED: number = 123123;
-const DEFAULT_WIDTH: number = 120;
-const DEFAULT_HEIGHT: number = 80;
-const DEFAULT_CELL_SIZE: number = 1;
+const DEFAULT_CELL_SIZE: number = 8;
+const DEFAULT_WIDTH: number = 120 * DEFAULT_CELL_SIZE;
+const DEFAULT_HEIGHT: number = 80 * DEFAULT_CELL_SIZE;
 const DEFAULT_ALPHA: number = 255;
 
-export function convert_index_to_xy(index: number, width: number): [number, number] {
-  const x = index % width;
-  const y = Math.floor(index / width);
+export function convert_index_to_xy(index: number, width: number, cell_size: number): [number, number] {
+  const adjusted_index = index * cell_size;
+  const x = adjusted_index % width;
+  const y = Math.floor(adjusted_index / width);
   return [x, y];
+}
+
+export function convert_xy_to_index(x: number, y: number, width: number, cell_size: number): number {
+  return y * width * cell_size + x;
 }
 
 export function calculate_cell_rgb(cell: number): [number, number, number] {
   let fill_color = 0x000000;
 
   if (is_tree(cell)) {
-    fill_color += 0x0000ff;
-  }
-
-  if (is_jack(cell)) {
     fill_color += 0x00ff00;
   }
 
-  if (is_bear(cell)) {
+  if (is_jack(cell)) {
     fill_color += 0xff0000;
+  }
+
+  if (is_bear(cell)) {
+    fill_color += 0x0000ff;
   }
 
   return [
@@ -37,20 +42,44 @@ export function calculate_cell_rgb(cell: number): [number, number, number] {
 }
 
 export function is_tree(cell: number): boolean {
-  return ((cell & 0x0000ff) >> 4 * 0) > 0;
+  return ((cell & 0x00ff) >> 4 * 0) > 0;
 }
 
 export function is_jack(cell: number): boolean {
-  return ((cell & 0x00ff00) >> 4 * 2) > 0;
+  return ((cell & 0x0f00) >> 4 * 2) > 0;
 }
 
 export function is_bear(cell: number): boolean {
-  return ((cell & 0xff0000) >> 4 * 3) > 0;
+  return ((cell & 0xf000) >> 4 * 3) > 0;
+}
+
+export function draw_pixel(
+  image_data: ImageData,
+  cell: number,
+  index: number,
+  width: number,
+  cell_size: number
+): void {
+  const [x, y] = convert_index_to_xy(index, width, cell_size);
+  const [r, g, b] = calculate_cell_rgb(cell);
+  const adjusted_index = convert_xy_to_index(x, y, width, cell_size);
+
+  for (let dy = 0; dy < cell_size; dy++) {
+    for (let dx = 0; dx < cell_size; dx++) {
+      const calculated_index = adjusted_index + dx + (dy * width);
+
+      image_data.data[4 * calculated_index + 0] = r;
+      image_data.data[4 * calculated_index + 1] = g;
+      image_data.data[4 * calculated_index + 2] = b;
+      image_data.data[4 * calculated_index + 3] = DEFAULT_ALPHA;
+    }
+  }
 }
 
 export function App(props: {}): ReactElement<any, any> {
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const [map, setMap] = useState<number[]>([]);
+  const [cellSize, setCellSize] = useState<number>(DEFAULT_CELL_SIZE);
   const [width, setWidth] = useState<number>(DEFAULT_WIDTH);
   const [height, setHeight] = useState<number>(DEFAULT_HEIGHT);
 
@@ -63,22 +92,14 @@ export function App(props: {}): ReactElement<any, any> {
     if (!ctx) return;
 
     let context = ctx;
-    context.clearRect(0, 0, width, height);
-
     let image_data = context.getImageData(0, 0, width, height);
 
     map.forEach((cell, index) => {
-      const [x, y] = convert_index_to_xy(index, width);
-      const [r, g, b] = calculate_cell_rgb(cell);
-
-      image_data.data[4 * index + 0] = r;
-      image_data.data[4 * index + 1] = g;
-      image_data.data[4 * index + 2] = b;
-      image_data.data[4 * index + 3] = DEFAULT_ALPHA;
+      draw_pixel(image_data, cell, index, width, cellSize);
     });
 
     context.putImageData(image_data, 0, 0);
-  }, [map]);
+  }, [map, width, height, cellSize]);
 
   return (
     <div id="App_container">
